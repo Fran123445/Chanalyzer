f2c9a95decb2126aa9d354e68f404f78a175833d
Franco Stazzone <fralstazzone@gmail.com>
2024-12-21 00:56:13 -0300

    Adds a method to retrieve boards with their replies

--- data_access/db_retriever.py
+++ data_access/db_retriever.py
@@ -1,17 +1,33 @@
 import pyodbc
 
+from models.models import Board, Thread
+
+
 class DbRetriever:
 
     def __init__(self, connection: pyodbc.Connection):
         self.connection = connection
         self.cursor = connection.cursor()
 
-    def get_threads(self, board_name: str, thread_amount: int):
-        rows = self.cursor.execute('EXEC uspGetTopThreads ?, ?', (board_name, thread_amount))
+    def _get_threads(self, board: Board, thread_amount: int):
+        rows = self.cursor.execute('EXEC uspGetTopThreads ?, ?', (board.board_name, thread_amount))
         return rows.fetchall()
 
-    def get_replies_from_thread(self, board_name: str, thread_number: int, reply_amount: int, min_words_per_reply: int):
-        self.cursor.execute("EXEC uspGetRepliesForThread ?, ?, ?, ?",
-                            (board_name, thread_number, reply_amount, min_words_per_reply))
-        rows = self.cursor.fetchall()
-        return rows
+    def _get_replies_from_thread(self, board: Board, thread_number: int, reply_amount: int, min_words_per_reply: int):
+        rows = self.cursor.execute("EXEC uspGetRepliesForThread ?, ?, ?, ?",
+                            (board.board_name, thread_number, reply_amount, min_words_per_reply))
+        return rows.fetchall()
+
+    def get_board_with_threads(self, board_name: str, thread_amount: int,
+                               reply_amount: int, min_words_per_reply: int):
+
+        board = Board(board_name, [])
+        threads = self._get_threads(board, thread_amount)
+
+        for thread in threads:
+            thread_number = int(thread[0])
+            thread_posts = self._get_replies_from_thread(board, thread_number, reply_amount, min_words_per_reply)
+            thread_posts = [post[0] for post in thread_posts]
+            board.threads.append(Thread(thread_number, thread_posts))
+
+        return board
