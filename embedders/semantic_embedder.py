896f367b29b8a9122ffa1fff47ecff8b6ced51a8
Franco Stazzone <fralstazzone@gmail.com>
2024-12-21 23:14:45 -0300

    Adds a distinct embedding method for string lists

--- embedders/semantic_embedder.py
+++ embedders/semantic_embedder.py
@@ -1,13 +1,29 @@
 from embedders.embedder import Embedder
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 class SemanticEmbedder(Embedder):
 
         def __init__(self, model: SentenceTransformer):
             self.model = model
 
-        def embed(self, text):
-            embeddings = self.model.encode(text)
+        def _get_weights(self, embeddings: np.array):
+            n = embeddings.shape[0]
+            positions = np.arange(1, n + 1)
+            weights = 1 / positions
 
-            return embeddings
+            return weights
 
+        def embed(self, text: str):
+            embedding = self.model.encode(text)
+
+            return embedding
+
+        def embed(self, text_list: list[str]):
+            """Generates the embedding of a list of texts by averaging the embeddings of the individual texts."""
+            embeddings = self.model.encode(text_list)
+
+            weights = self._get_weights(embeddings)
+            average_embedding = np.average(embeddings, axis=0, weights=weights)
+
+            return average_embedding
