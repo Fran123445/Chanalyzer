324fbd1777ee7173c26dc25e14ae9c40adc6258c
Franco Stazzone <fralstazzone@gmail.com>
2024-12-22 20:05:01 -0300

    Adds methods to calculate thread and board embeddings

--- embedders/semantic_embedder.py
+++ embedders/semantic_embedder.py
@@ -1,6 +1,7 @@
 from embedders.embedder import Embedder
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from models.models import Thread, Board
 
 class SemanticEmbedder(Embedder):
 
@@ -14,6 +15,12 @@ class SemanticEmbedder(Embedder):
 
             return weights
 
+        def _get_average_embedding(self, embeddings: np.array):
+            weights = self._get_weights(embeddings)
+            average_embedding = np.average(embeddings, axis=0, weights=weights)
+
+            return average_embedding
+
         def embed(self, text: str):
             embedding = self.model.encode(text)
 
@@ -23,7 +30,25 @@ class SemanticEmbedder(Embedder):
             """Generates the embedding of a list of texts by averaging the embeddings of the individual texts."""
             embeddings = self.model.encode(text_list)
 
-            weights = self._get_weights(embeddings)
-            average_embedding = np.average(embeddings, axis=0, weights=weights)
+            average_embedding = self._get_average_embedding(embeddings)
 
             return average_embedding
+
+        def calculate_thread_embedding(self, thread: Thread):
+            thread_posts = thread.posts
+            thread_embedding = self.embed(thread_posts)
+
+            thread.semantic_embedding = thread_embedding
+
+        def calculate_board_embedding(self, board: Board):
+            thread_embeddings = []
+
+            for thread in board.threads:
+                if thread.semantic_embedding is None:
+                    self.calculate_thread_embedding(thread)
+
+                thread_embeddings.append(thread.semantic_embedding)
+
+            board_embedding = self._get_average_embedding(np.array(thread_embeddings))
+
+            board.semantic_embedding = board_embedding
