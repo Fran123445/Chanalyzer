50e29481b1dc47fc332bb06d8233d3833d64db54
Franco Stazzone <fralstazzone@gmail.com>
2024-12-23 18:59:49 -0300

    Adds a processor to calculate thread and board embeddings

--- embedders/semantic_embedder.py
+++ embedders/semantic_embedder.py
@@ -1,12 +1,12 @@
 from embedders.embedder import Embedder
 from sentence_transformers import SentenceTransformer
-import numpy as np
-from models.models import Thread, Board
+from utils.aggregator import Aggregator
 
 class SemanticEmbedder(Embedder):
 
-        def __init__(self, model: SentenceTransformer):
+        def __init__(self, model: SentenceTransformer, aggregator: Aggregator):
             self.model = model
+            self.aggregator = aggregator
 
         def embed(self, text: str):
             embedding = self.model.encode(text)
@@ -17,31 +17,6 @@ class SemanticEmbedder(Embedder):
             """Generates the embedding of a list of texts by averaging the embeddings of the individual texts."""
             embeddings = self.model.encode(text_list)
 
-            average_embedding = self._get_average_embedding(embeddings)
+            average_embedding = self.aggregator.get_average_embedding(embeddings)
 
             return average_embedding
-
-        def calculate_thread_embedding(self, thread: Thread):
-            thread_posts = thread.posts
-            title = thread.thread_title
-
-            # I know it's technically not a post but still
-            if title is not None:
-                thread_posts.insert(0, title)
-
-            thread_embedding = self.embed(thread_posts)
-
-            thread.semantic_embedding = thread_embedding
-
-        def calculate_board_embedding(self, board: Board):
-            thread_embeddings = []
-
-            for thread in board.threads:
-                if thread.semantic_embedding is None:
-                    self.calculate_thread_embedding(thread)
-
-                thread_embeddings.append(thread.semantic_embedding)
-
-            board_embedding = self._get_average_embedding(np.array(thread_embeddings))
-
-            board.semantic_embedding = board_embedding
---
+++ processor/processor.py
@@ -0,0 +1,34 @@
+from embedders.embedder import Embedder
+from utils.aggregator import Aggregator
+from models.models import Thread, Board
+import numpy as np
+
+class Processor:
+    def __init__(self, embedder: Embedder, aggregator: Aggregator):
+        self.embedder = embedder
+        self.aggregator = aggregator
+
+    def calculate_thread_embedding(self, thread: Thread):
+        thread_posts = thread.posts
+        title = thread.thread_title
+
+        # I know it's technically not a post but still
+        if title is not None:
+            thread_posts.insert(0, title)
+
+        thread_embedding = self.embedder.embed(thread_posts)
+
+        thread.semantic_embedding = thread_embedding
+
+    def calculate_board_embedding(self, board: Board):
+        thread_embeddings = []
+
+        for thread in board.threads:
+            if thread.semantic_embedding is None:
+                self.calculate_thread_embedding(thread)
+
+            thread_embeddings.append(thread.semantic_embedding)
+
+        board_embedding = self.aggregator.get_average_embedding(np.array(thread_embeddings))
+
+        board.semantic_embedding = board_embedding
