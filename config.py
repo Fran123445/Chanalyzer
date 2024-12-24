a9584660a135c8121f36501b02be5683971873b1
Franco Stazzone <fralstazzone@gmail.com>
2024-12-23 23:40:22 -0300

    Adds a config file

---
+++ config.py
@@ -0,0 +1,10 @@
+SQL_DB_DRIVER= # e.g. 'ODBC Driver 17 for SQL Server'
+SQL_DB_SERVER= # e.g. 'Username\SQLEXPRESS'
+SQL_DB_NAME= # e.g. 'BoardCrawler'
+
+MONGO_DB_SERVER= # e.g. 'mongodb://localhost:27017/'
+MONGO_DB_NAME= # e.g. 'Chanalyzer'
+MONGO_COLLECTION_BOARDS= # e.g. 'Boards'
+MONGO_COLLECTION_THREADS= # e.g. 'Threads'
+
+TRANSFORMER_MODEL_NAME= # e.g. 'distiluse-base-multilingual-cased-v1'
