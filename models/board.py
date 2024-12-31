a064956452725e72080a2f888eeef02c61af99a1
Franco Stazzone <fralstazzone@gmail.com>
2024-12-31 20:46:18 -0300

    Corrects embedding datatype

--- models/board.py
+++ models/board.py
@@ -1,12 +1,14 @@
 from typing import Optional
 
+import numpy as np
+
 from models.thread import Thread
 
 class Board:
 
     def __init__(self, board_name: str,
                  threads: Optional[list[Thread]] = None,
-                 semantic_embedding: Optional[tuple[float]] = None):
+                 semantic_embedding: Optional[np.ndarray] = None):
 
         self.board_name = board_name
         self.threads = threads
--- models/thread.py
+++ models/thread.py
@@ -1,12 +1,15 @@
 from typing import Optional
 
+import numpy as np
+
+
 class Thread:
 
     def __init__(self, board: str,
                  thread_number: int,
                  thread_title: Optional[str] = None,
                  posts: Optional[list[str]] = None,
-                 semantic_embedding: Optional[tuple[float]] = None):
+                 semantic_embedding: Optional[np.ndarray] = None):
 
         self.board = board
         self.thread_number = thread_number
