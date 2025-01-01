77d5cd5581540350ad6dfaf663170110f07a17ce
Franco Stazzone <fralstazzone@gmail.com>
2025-01-01 19:47:41 -0300

    Adds methods to find a board given a text/list of texts

--- services/board_finder/board_finder.py
+++ services/board_finder/board_finder.py
@@ -12,10 +12,25 @@ class BoardFinder:
         self.semantic_embedder = semantic_embedder
         self.matcher = matcher
 
+
     def find_similar_to_board(self, board_name: str, top_n: int):
         board = self.mongo_access.get_board(board_name)
         board_embedding = board.semantic_embedding
 
         top_similar_boards = self.matcher.get_top_similar_boards(board_embedding, "semantic_embedding", top_n)
+
+        return top_similar_boards
+
+    def find_similar_to_text(self, text: str, top_n: int):
+        text_embedding = self.semantic_embedder.embed_text(text)
+
+        top_similar_boards = self.matcher.get_top_similar_boards(text_embedding, "semantic_embedding", top_n)
+
+        return top_similar_boards
+
+    def find_similar_to_text_list(self, text_list: list[str], top_n: int):
+        text_embedding = self.semantic_embedder.embed_text_list(text_list)
+
+        top_similar_boards = self.matcher.get_top_similar_boards(text_embedding, "semantic_embedding", top_n)
 
         return top_similar_boards
