43901a131dd1cce0ba85ebdae677351d98e7a207
Franco Stazzone <fralstazzone@gmail.com>
2025-01-01 19:43:34 -0300

    Adds a method to find boards similar to a given board

--- services/board_finder/board_finder.py
+++ services/board_finder/board_finder.py
@@ -11,3 +11,11 @@ class BoardFinder:
         self.mongo_access = mongo_access
         self.semantic_embedder = semantic_embedder
         self.matcher = matcher
+
+    def find_similar_to_board(self, board_name: str, top_n: int):
+        board = self.mongo_access.get_board(board_name)
+        board_embedding = board.semantic_embedding
+
+        top_similar_boards = self.matcher.get_top_similar_boards(board_embedding, "semantic_embedding", top_n)
+
+        return top_similar_boards
