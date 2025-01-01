45cdbb22985b3b215a775375eb40b22369741bbf
Franco Stazzone <fralstazzone@gmail.com>
2024-12-31 21:45:55 -0300

    Casts embedding to np.float64 for PyTorch compatibility

--- services/embedders/semantic_embedder.py
+++ services/embedders/semantic_embedder.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from services.embedders.embedder import Embedder
 from sentence_transformers import SentenceTransformer
 from services.utils.aggregator import Aggregator
@@ -9,14 +11,14 @@ class SemanticEmbedder(Embedder):
             self.aggregator = aggregator
 
         def embed_text(self, text: str):
-            embedding = self.model.encode(text)
+            embedding = self.model.encode(text, convert_to_numpy=True)
 
-            return embedding
+            return embedding.astype(np.float64)
 
         def embed_text_list(self, text_list: list[str]):
             """Generates the embedding of a list of texts by averaging the embeddings of the individual texts."""
-            embeddings = self.model.encode(text_list)
+            embeddings = self.model.encode(text_list, convert_to_numpy=True)
 
             average_embedding = self.aggregator.get_average_embedding(embeddings)
 
-            return average_embedding
+            return average_embedding.astype(np.float64)
