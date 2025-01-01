5186d75f8b338e71da9079d85623e7764c19c6ad
Franco Stazzone <fralstazzone@gmail.com>
2024-12-31 21:13:11 -0300

    Generalizes calculate_similarities method

--- services/matcher/matcher.py
+++ services/matcher/matcher.py
@@ -7,20 +7,21 @@ class Matcher:
     def __init__(self, mongo_access: MongoAccess):
         self.mongo_access = mongo_access
 
-    def calculate_similarities(self, embedding: list, embedding_type: str):
-        boards = self.mongo_access.get_boards()
-
+    def calculate_similarities(self, embedding: list, element_list: list, embedding_type: str):
         similarity_dict = {}
 
-        for board in boards:
-            similarity = sentence_transformers.util.cos_sim(embedding, getattr(board, embedding_type))
-            similarity_dict[board.name] = similarity
+        for element in element_list:
+            similarity = sentence_transformers.util.cos_sim(embedding, getattr(element, embedding_type))
+            similarity_dict[element] = similarity
 
         return similarity_dict
 
     def get_top_similar_boards(self, embedding: list, embedding_type: str, top_n: int):
-        similarity_dict = self.calculate_similarities(embedding, embedding_type)
+        boards = self.mongo_access.get_boards()
+
+        similarity_dict = self.calculate_similarities(embedding, boards, embedding_type)
 
         top_similar_boards = sorted(similarity_dict.items(), key=lambda x: x[1], reverse=True)[:top_n]
+        top_similar_boards = [(board.board_name, similarity) for board, similarity in top_similar_boards]
 
         return top_similar_boards
