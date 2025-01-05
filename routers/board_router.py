ce09c2b046adfe98084e04b6c77fb83aad26cecd
Franco Stazzone <fralstazzone@gmail.com>
2025-01-05 18:22:42 -0300

    Adds endpoints for getting similarities

--- routers/board_router.py
+++ routers/board_router.py
@@ -8,3 +8,11 @@ router = APIRouter(
 def post_embedding(board_name: str, request: Request):
     request.app.state.semantic_processor.process_board(board_name)
     return {"message": f"{board_name} processed"}
+
+@router.get("/{board_name}/similar/semantic")
+def get_similar_to_board(board_name: str, request:Request, top_n = 10):
+    return request.app.state.board_finder.find_similar_to_board(board_name, top_n)
+
+@router.get("/similar/semantic")
+def get_similar_to_texts(text_list: list[str], request: Request, top_n = 10):
+    return request.app.state.board_finder.find_similar_to_text_list(text_list, top_n)
