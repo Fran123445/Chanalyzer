66dc22cf2224ec3f2e9c5b2d1e7767713bd41e1e
Franco Stazzone <fralstazzone@gmail.com>
2025-01-14 20:29:18 -0300

    Adds type hinting

--- routers/board_router.py
+++ routers/board_router.py
@@ -10,13 +10,13 @@ def post_embedding(board_name: str, request: Request):
     return {"message": f"{board_name} processed"}
 
 @router.get("/{board_name}/similar/semantic")
-def get_similar_to_board(board_name: str, request:Request, top_n = 10):
+def get_similar_to_board(board_name: str, request:Request, top_n: int = 10):
     return request.app.state.board_finder.find_similar_to_board(board_name, top_n)
 
 @router.post("/similar/semantic/simple")
-def get_similar_to_text_list(text_list: list[str], weights_list: list[int], request: Request, top_n = 10):
+def get_similar_to_text_list(text_list: list[str], weights_list: list[int], request: Request, top_n: int = 10):
     return request.app.state.board_finder.find_similar_to_text_list(text_list, weights_list, top_n)
 
 @router.post("/similar/semantic/multiple")
-def get_similar_to_multiple_text_lists(text_lists: list[list[str]], weight_lists: list[list[int]], request: Request, top_n = 10):
+def get_similar_to_multiple_text_lists(text_lists: list[list[str]], weight_lists: list[list[int]], request: Request, top_n: int = 10):
     return request.app.state.board_finder.find_similar_to_multiple_text_lists(text_lists, weight_lists, top_n)
